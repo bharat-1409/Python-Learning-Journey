0c9f35c01a01843d6185a2d02424d618b95c5678
Bharat dutta <bharatdutta11j@gmail.com>
2026-06-06 13:18:35 +0530

    Day 1: Variables and Data Types in Python

---
+++ Chapter-01-Variables-And-DataTypes/variables_datatypes.py
@@ -0,0 +1,59 @@
+print("HELLO WORLD") # Without this we can't start our learning😂
+
+# Hashtag is used for the single line comment in python languageT
+
+"""{THIS IS MULTILINE COMMENT}""" 
+
+#VARIABLES- We can take any name in variable name
+Company = 12
+print(Company)
+
+Amount = 1020
+print(Amount)
+
+#Types of cases we used for creating variable name
+
+# 1. camel case
+bharatDutta = 12
+print(bharatDutta)
+
+#2. pascal case
+MyVariableName = 1000
+print(MyVariableName)
+
+#3. snake case 
+Bharat_dutta = 1222222
+print(Bharat_dutta)
+
+#We mostly prefer the snake case among above three of them.
+
+#DATATYPES-
+ 
+# 1. int datatype - 0 to infinity and -1 to infinity
+
+a = 23
+b = -1111
+print(type(a))
+print(type(b)) #type helps to find which data type is used here
+
+# 2. float datatype(fraction and decimal form)
+
+hello = 123.555
+print(type(hello)) #type helps to find which data type is used here
+hi = 122/3
+print(type(hi))
+
+# 3.  string datatype(amuything inside the codes)
+
+name = "akarsh is a good person in the university" # we use double or single quotes in the string okay and for textual data
+print(name)
+print(type(name))
+
+# 4. boolean datatype
+a = True # always capital letter for true and false in boolean datatype
+b = False
+print(type(a))
+
+
+
+
